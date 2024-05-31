ead3f03380533045fe41f35ab4118180b492575a
qinaichen <455905502@qq.com>
2024-05-31 17:20:26 +0800

    登录

--- shiro-ini-sample/src/main/java/com/etena/cloud/servlet/LoginServlet.java
+++ shiro-ini-sample/src/main/java/com/etena/cloud/servlet/LoginServlet.java
@@ -1,6 +1,9 @@
 package com.etena.cloud.servlet;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 
@@ -31,6 +34,21 @@ public class LoginServlet extends HttpServlet {
 		String password = request.getParameter("password");
 		Subject subject = SecurityUtils.getSubject();
 		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-		subject.login(token);
+		String emsg = null;
+		try {
+			subject.login(token);
+		}catch (UnknownAccountException e) {
+			emsg = "用户名出错";
+		}catch (IncorrectCredentialsException e) {
+			emsg = "密码出错";
+		}catch (AuthenticationException e) {
+			emsg = "其他问题: " + e.getMessage();
+		}
+		if((emsg != null) ) {
+			request.setAttribute("emsg", emsg);
+			request.getRequestDispatcher("/login.jsp").forward(request, response);
+		}else{
+			response.sendRedirect(request.getContextPath() + "/");
+		}
 	}
 }
